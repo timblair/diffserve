4b8061c55bfee78205ab3247f065e1aa5b09838c
Tim Blair <tim@bla.ir>
2011-06-10 19:05:16 +0100

    Define proper layout and styling (thank you, Resque.)

--- lib/diffserve/server.rb
+++ lib/diffserve/server.rb
@@ -18,21 +18,9 @@ module DiffServe
     end
 
     get "/" do
-      repo = DiffServe::Repository.locate
-      unified = Diff::Display::Unified.new(repo.diff.result)
-      out = <<-EOS
-        <style>
-          *         { font-family: monospace; }
-          td        { white-space: nowrap; }
-          td.header { font-weight: bold; font-size: 150%; padding: 1.5em 0 1em; }
-          td.insert { background-color: #CFC; }
-          td.delete { background-color: #FCC; }
-          td.ln     { padding: 0 0.3em; font-family: monospace; color: #999; }
-        </style>
-      EOS
-      out << "<h1>" + repo.path + "</h1>"
-      out << "<table>" + unified.render(DiffServe::Renderer.new) + "</table>"
-      out
+      @repo = DiffServe::Repository.locate
+      @unified = Diff::Display::Unified.new(@repo.diff.result)
+      erb :overview
     end
 
   end
